4582d5e26f60084f012ef60252ceb0bfab9e143c
Kami Boers <kamiboers@gmail.com>
2016-02-13 21:35:35 -0700

    added timer, excluded sorted values

--- enigma/bubblier.rb
+++ enigma/bubblier.rb
@@ -3,45 +3,66 @@ require 'pry'
 class BubblierSort
 
 def initialize
-  @sorts = 0
+  @switches = 0
 end
 
 def sort(array)
+  print_original(array)
+  timer_start
+  @num_fewer_comparisons = 0
     until @sorted == true
       @sorted = true
       traverse_array(array)
     end
-  puts array.to_s
-  puts "Number of sorts: #{@sorts}"
+    timer_stop
+    print_final(array)
+end
+
+def print_original(array)
+  puts "\nScrambled values:\n #{array.to_s}"
 end
 
 def traverse_array(array)
   sorted = 0
-  (0...array.length).each do |pos|
-    compare(array, pos)
+  num_unsorted = array.length - @num_fewer_comparisons
+  (0...num_unsorted).each do |pos|
+    compare_neighboring_values(array, pos)
   end
-  sorted
+  @num_fewer_comparisons += 1
 end
 
-def compare(array, pos)
+def compare_neighboring_values(array, pos)
   if pos < array.length-1 && array[pos] > array[pos+1]
-    switch_neighbor_order(array, pos)
-    one_sort_occurs
+    switch_neighboring_value_order(array, pos)
+    one_value_switch_occurs
   end
 end
 
-def one_sort_occurs
-  @sorted = false
-  @sorts += 1
-end
-
-def switch_neighbor_order(array, pos=0)
+def switch_neighboring_value_order(array, pos=0)
   array[pos], array[pos+1] = array[pos+1], array[pos]
 end
 
+def one_value_switch_occurs
+  @sorted = false
+  @switches += 1
+end
+
+def print_final(array)
+  puts "\nSorted values:\n #{array.to_s}"
+  puts "\nNumber of value switches: #{@switches}"
+  puts "#{@time_elapsed} seconds elapsed during sorting.\n"
+end
+
+def timer_start
+  @time = Time.now
+end
+
+def timer_stop
+  @time_elapsed = Time.now - @time
+end
 
 end
 
 test = BubblierSort.new
-puts array = (1..100).to_a.shuffle
-test.sort(array)
+test.sort((1..10000).to_a.shuffle)
+test.sort(['a', 'c', 'd', 'A', 'a', 'f'])
