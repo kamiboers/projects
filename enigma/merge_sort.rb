72a66a1e68f729d1aae91aa66ff74619ba53b3ef
Kami Boers <kamiboers@gmail.com>
2016-02-15 17:40:45 -0700

    merge minor changes

--- enigma/merge_sort.rb
+++ enigma/merge_sort.rb
@@ -1,41 +1,41 @@
- require 'pry'
+require 'pry'
 
- class MergeSort
+class MergeSort
 
-   def initialize
-   end
+  def initialize
+  end
 
-   def sort(array)
+  def sort(array)
 
-     n = array.length
-     return array if n <= 1
-     midpoint = n/2
-      left_array = array[0...midpoint]
-      right_array = array[midpoint..n]
+    n = array.length
+    return array if n == 1
+    midpoint = n/2
 
-      left = sort(left_array).to_a.compact
-      right = sort(right_array).to_a.compact
+     left_array = array[0...midpoint]
+     right_array = array[midpoint..n]
 
-      binding.pry
-      # merge(left, right)
-   end
+     left = sort(left_array)
+     right = sort(right_array)
 
-   def merge(left, right)
-     sorted =[]
-     binding.pry
+    #  merge(left, right)
 
-     until left.empty? || right.empty?
-       if left[0] <= right[0]
-         sorted << left.shift
-       elsif right[0] <= left[0]
-         sorted << right.shift
-       else
-         sorted << right.shift
-         sorted << left.shift
-       end
-     end
-     puts (sorted + left + right).flatten
-   end
+  end
+
+  def merge(left, right)
+    sorted =[]
+
+    until left.empty? || right.empty?
+      if left[0] <= right[0]
+        sorted << left.shift
+      elsif right[0] <= left[0]
+        sorted << right.shift
+      else
+        sorted << right.shift
+        sorted << left.shift
+      end
+    end
+    puts (sorted + left + right)
+  end
 
 
 end
