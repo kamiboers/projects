f4e30b39381478acd444f907365b0bce79d2386f
Kami Boers <kamiboers@gmail.com>
2016-02-15 22:34:16 -0700

    final versions of bubbles

--- enigma/bubblier.rb
+++ enigma/bubblier.rb
@@ -14,18 +14,18 @@ def sort(array)
   @num_fewer_comparisons = 0
     until @sorted == true
       @sorted = true
-      traverse_array(array)
+      traverse(array)
     end
     timer_stop
     print_final(array)
+    return array
 end
 
 def display_original(array)
   "\nScrambled values:\n #{array.to_s}"
 end
 
-def traverse_array(array)
-  sorted = 0
+def traverse(array)
   num_unsorted = array.length - @num_fewer_comparisons
   (0...num_unsorted).each do |pos|
     compare_neighboring_values(array, pos)
@@ -33,19 +33,22 @@ def traverse_array(array)
   @num_fewer_comparisons += 1
 end
 
-def compare_neighboring_values(array, pos)
+def compare_neighboring_values(array, pos=0)
   if neighbors_out_of_order?(array, pos)
-    switch_neighboring_value_order(array, pos)
     one_value_switch_occurs
+    switch_neighboring_value_order(array, pos)
+  else
+    return array
   end
 end
 
-def neighbors_out_of_order?(array, pos)
+def neighbors_out_of_order?(array, pos=0)
   pos < array.length-1 && array[pos] > array[pos+1]
 end
 
 def switch_neighboring_value_order(array, pos=0)
   array[pos], array[pos+1] = array[pos+1], array[pos]
+
 end
 
 def one_value_switch_occurs
@@ -74,5 +77,5 @@ if __FILE__ == $0
 
 
 test = BubbleSort.new
-test.sort((1..10).to_a.shuffle)
+test.sort((1..100).to_a.shuffle)
 end
